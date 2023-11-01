fddff483444be1c24661afffb577191df165dc31
kushidam <masaki.kushida.var@gmail.com>
2023-11-01 23:14:12 +0900

    [Add] CLI

--- cmd/main.go
+++ cmd/main.go
@@ -1,26 +1,34 @@
 package main
 
 import (
+	"flag"
 	"os"
 
 	"github.com/nullrocks/identicon"
 )
 
+
+var (
+	namespace     = flag.String("namespace", "github", "use namespace")
+	blocksSize      = flag.Int("size", 5, "use number of blocks (Size)")
+	density    = flag.Int("density", 3, "use density")
+	userText    = flag.String("text", "nullrocks", "use decides the resulting figure")
+)
+
 func main(){
-	
+	flag.Parse()
 	// New Generator: Rehuse 
 	ig, err := identicon.New(
-		"github", // Namespace
-		5,        // Number of blocks (Size)
-		3,        // Density
+		*namespace, // Namespace
+		*blocksSize,        // Number of blocks (Size)
+		*density,        // Density
 	)
 	
 	if err != nil {
 		panic(err) // Invalid Size or Density
 	}
 	
-	username := "nullrocks"      // Text - decides the resulting figure
-	ii, err := ig.Draw(username) // Generate an IdentIcon
+	ii, err := ig.Draw(*userText) // Generate an IdentIcon
 	
 	if err != nil {
 		panic(err) // Text is empty
